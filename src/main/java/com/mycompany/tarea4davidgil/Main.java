c3dc27d0f3f5a9b54a7bdb8ae619f75f6b4c3afa
DanielRasero <danielraseromarin@gmail.com>
2020-04-26 17:50:02 +0200

    Clase Main

---
+++ src/main/java/com/mycompany/tarea4davidgil/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.tarea4davidgil;
+
+/**
+ *
+ * @author danie
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Animal a1= new  Animal(4,"Pipo","perro");
+        Animal a2= new  Animal(4,"Mishi","gato");
+        
+        System.out.println(a1.toString());
+        System.out.println(a2.toString());
+        
+    }
+    
+}
