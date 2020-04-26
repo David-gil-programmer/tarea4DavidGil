56cef4e060fb32c75dfd0e747ce0b34a927269ef
David <davidgilm123@gmail.com>
2020-04-26 17:18:01 +0200

    Añade atributos en clase Animal

---
+++ src/main/java/com/mycompany/tarea4davidgil/Animal.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.tarea4davidgil;
+
+/**
+ *
+ * @author david
+ */
+public class Animal {
+    private int numpatas;
+    private String nombre;
+    private String tipo;
+    
+}
